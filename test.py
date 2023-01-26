03ea2458c7395e1e1cddaf053ef08af3bef48416
isurumanoddev <111575402+isurumanoddev@users.noreply.github.com>
2023-01-26 13:02:01 +0530

    python_for_everybody_episode13

--- test.py
+++
@@ -1,8 +0,0 @@
-import urllib.request,urllib.parse,urllib.error
-
-# urlopen = urllib.request.urlopen("http://maps.googleapis.com/maps/api/geocode/json")
-# for line in urlopen:
-#     print(line.decode().strip())
-
-serviceurl = "http://maps.googleapis.com/maps/api/geocode/json?"
-
